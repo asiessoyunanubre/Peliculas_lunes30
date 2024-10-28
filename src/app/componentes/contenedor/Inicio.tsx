f2d1caaa3352d1fa0e17e6453ca06b6265d56ded
carlosCorrea <ccorrea14@cuc.edu.co>
2024-10-28 10:19:17 -0500

    version02

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -4,9 +4,16 @@ export const Inicio = () => {
       <div className="pt-4 d-flex justify-content-around">
         <div className="col-md-6">
           <div className="h-100 p-5 text-bg-dark rounded-3">
+            <h2> El mejor sitio de peliculas</h2>
+            <p>
+                Las mejores peliculas donde encontraras todas las peliculas
+            </p>
+            <button className="btn btn-outline-light" type="button">
+                Buscar Peliculas 
+            </button>
           </div>
         </div>
       </div>
     </>
   );
-};
+}
